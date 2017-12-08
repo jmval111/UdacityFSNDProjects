30d572516ed9c2c5d33db19d4eac33c606a2708b
Viraj Bhosale <viraj.bhosale@macys.com>
2017-12-09 01:12:53 +0530

    Changes according to the Udacity Javascript style guide

--- NearbyRestaurantsMap/js/thirdPartyAPI.js
+++ NearbyRestaurantsMap/js/thirdPartyAPI.js
@@ -1,14 +1,12 @@
-ZOMATO_API_KEY = 'PASTE_ZOMATO_KEY';
-ZOMATO_BASE_URI = "https://developers.zomato.com/api/v2.1/search";
-DEFAULT_QUERY_PARAM = {count: 20, sort: "real_distance", order: "asc"};//, sort: "real_distance", order: "asc"};
-var xhrCap;
-var errorCap;
-var tpData;
+const ZOMATO_API_KEY = 'ZOMATO_API_KEY';
+const ZOMATO_BASE_URI = "https://developers.zomato.com/api/v2.1/search";
+const DEFAULT_QUERY_PARAM = {count: 20, sort: "real_distance", order: "asc"};
+
 function fetchRestaurantsInCircle(lat, lng, radius, callbackFn, statusChangeHandlerFn){
 
   console.log("lat:"+lat+", lng:"+lng+", radius:"+radius)
   //create copy of DEFAULT_QUERY_PARAM
-  var queryParam = Object.assign({},DEFAULT_QUERY_PARAM);
+  let queryParam = Object.assign({},DEFAULT_QUERY_PARAM);
   queryParam.radius = encodeURIComponent(radius);
   queryParam.lat = encodeURIComponent(lat);
   queryParam.lon = encodeURIComponent(lng);
@@ -28,9 +26,6 @@ function fetchRestaurantsInCircle(lat, lng, radius, callbackFn, statusChangeHand
   }
 
   function wrapData(data, textStatus, xhr){
-    //TODO for debug, remove later
-    tpData = data;
-    xhrCap = xhr;
     console.log('wrapData');
     data.isError = false;
     callbackFn(data);
@@ -39,10 +34,6 @@ function fetchRestaurantsInCircle(lat, lng, radius, callbackFn, statusChangeHand
 
   function handleAjaxError(xhr, textStatus, error) {
     statusChangeHandlerFn(xhr, error);
-
-    //TODO for debug, remove later
-    xhrCap = xhr;
-    errorCap = error;
   }
 
 }
